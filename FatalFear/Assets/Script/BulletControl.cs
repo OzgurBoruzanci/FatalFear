ac419ebf5794049568bdb3541a583aa5c0828b83
OzgurBoruzanci <ozgurboruzanc@gmail.com>
2022-11-25 00:38:00 +0300

    2.0

---
+++ FatalFear/Assets/Script/BulletControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+    }
+}
--- FatalFear/Assets/Script/CharacterControl.cs
+++ FatalFear/Assets/Script/CharacterControl.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    int bulletPoolCounter = 0;
     float horizontal=0, vertical=0;
     float headRotUpDown=0, headRotLefRight=0;
     public float speed = 0;
 
-    bool shootControl = false;
+    bool shotControl = false;
 
+    Coroutine memoryCoroutine=null;
     Transform skeleton;
     Rigidbody physics;
     Animator animator;
@@ -17,8 +19,14 @@ public class CharacterControl : MonoBehaviour
     public GameObject headCamera;
     public GameObject sight;
     public Vector3 offset;
+    public RuntimeAnimatorController onShot;
+    public RuntimeAnimatorController notShot;
+    public GameObject bullet;
+    public Transform bulletPosition;
+    public GameObject[] bulletPool;
 
     RaycastHit hit;
+    RaycastHit hitFire;
     Vector3 distanceBetweenCamera;
 
 
@@ -32,11 +40,32 @@ public class CharacterControl : MonoBehaviour
         pos2 = headCamera.transform.Find("Pos2").gameObject;
 
         skeleton = animator.GetBoneTransform(HumanBodyBones.Chest);
+
+        BulletPool();
+    }
+
+    void BulletPool()
+    {
+        bulletPool=new GameObject[10];
+        for (int i = 0; i < bulletPool.Length; i++)
+        {
+            GameObject bulletObj = Instantiate(bullet);
+            bulletObj.SetActive(false);
+            bulletPool[i]=bulletObj;
+        }
     }
 
     private void LateUpdate()
     {
-        skeleton.rotation *= Quaternion.Euler(offset);
+        if (shotControl)
+        {
+            if (hitFire.distance>3)
+            {
+                skeleton.LookAt(hitFire.point);
+                skeleton.rotation *= Quaternion.Euler(offset);
+            }
+        }
+        
     }
 
     private void Update()
@@ -47,37 +76,77 @@ public class CharacterControl : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            speed *= 2;
+            if (!shotControl)
+            {
+                speed *= 2;
+            }
             animator.SetBool("RunParameters", true);
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            speed/=2;
+            if (!shotControl)
+            {
+                speed/=2;
+            }
             animator.SetBool("RunParameters", false);
         }
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             sight.SetActive(true);
-            shootControl = true;
+            shotControl = true;
         }
         else if (Input.GetKeyUp(KeyCode.Mouse1))
         {
             sight.SetActive(false);
-            shootControl = false;
+            shotControl = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (memoryCoroutine!=null)
+            {
+                StopCoroutine(memoryCoroutine);
+            }
+            animator.SetBool("FireParameters", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            memoryCoroutine = StartCoroutine(FireAnimStop());
         }
     }
+    IEnumerator FireAnimStop()
+    {
+        yield return new WaitForSeconds(0.2f);
+        animator.SetBool("FireParameters", false);
+    }
 
+    void Shot()
+    {
+        bulletPool[bulletPoolCounter].SetActive(true);
+        bulletPool[bulletPoolCounter].transform.position = bulletPosition.position;
+        bulletPool[bulletPoolCounter].transform.rotation = bulletPosition.rotation;
+
+        Rigidbody bulletPhysics = bulletPool[bulletPoolCounter].GetComponent<Rigidbody>();
+        bulletPhysics.velocity = Vector2.zero;
+        bulletPhysics.AddForce((hitFire.point - bulletPosition.transform.position).normalized * 1000);
+        bulletPoolCounter++;
+        if (bulletPoolCounter == bulletPool.Length)
+        {
+            bulletPoolCounter = 0;
+        }
+    }
 
     void FixedUpdate()
     {
         Move();
-        if (!shootControl)
+        if (!shotControl)
         {
+            animator.runtimeAnimatorController = notShot;
             mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, pos1.transform.position, 0.1f);
             RotationCamera();
         }
         else
         {
+            animator.runtimeAnimatorController = onShot;
             mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, pos2.transform.position, 0.1f);
             RotationCamera2();
         }
@@ -104,6 +173,8 @@ public class CharacterControl : MonoBehaviour
         Physics.Raycast(Vector3.zero, headCamera.transform.GetChild(0).forward, out hit);
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(hit.point.x, 0, hit.point.z)), 0.4f);
 
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        Physics.Raycast(ray, out hitFire);
     }
     void RotationCamera()
     {
